71248526e87159c6a40472270cf030d6b74cee9e
Misael Hernandez <misahergonz82@gmail.com>
2020-12-01 15:56:19 -0600

    Puerto Modif

--- index.js
+++ index.js
@@ -8,19 +8,15 @@ const  app = express();
  /// habilitando express . JSON
  app.use(express.json({extended: true}));
 
-/// para el ruteo...
+
+///puerto d ela app
+ const port  = process.env.PORT || 4000;
+ ///definiendo prinicpal
+ /// para el ruteo...
  app.use('/api/revision',require('./routes/revision'))
 
  //....
 
-///puerto d ela app
- const port  = process.env.port || 4000;
- ///definiendo prinicpal
-   app.get('/',(req,res)=>{
-         res.send('Hola Mundo');
-   });
-
- /////
 
  app.listen(port,'0.0.0.0',()=>{
         console.log('El servidro esta funcionando en el puerto--> ',port);
